a58dfb2429e27c46029eb51eed3a43e8f3b0a69b
maxcoh3n <maxcoh3n@gmail.com>
2021-02-23 23:01:54 -0800

    add more tests tp part 1

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -3,22 +3,25 @@ const {formatVolumeIconPath} = require('../assets/scripts/main');
 
 
 describe('FormatVolumeIconPath', ()=>{
-    test('is at maximum', ()=>{
+    test('is at maximum at 100', ()=>{
         expect(formatVolumeIconPath(100)).toMatch(/volume-level-3/);
     });
-    test('is at maximum', ()=>{
+    test('is at maximum at 67', ()=>{
         expect(formatVolumeIconPath(67)).toMatch(/volume-level-3/);
     });
-    test('is at high', ()=>{
+    test('is at high at 66', ()=>{
         expect(formatVolumeIconPath(66)).toMatch(/volume-level-2/);
     });
-    test('is at high', ()=>{
+    test('is at high at 34', ()=>{
         expect(formatVolumeIconPath(34)).toMatch(/volume-level-2/);
     });
-    test('is at low', ()=>{
+    test('is at low at 33', ()=>{
+        expect(formatVolumeIconPath(33)).toMatch(/volume-level-1/);
+    });
+    test('is at low at 1', ()=>{
         expect(formatVolumeIconPath(1)).toMatch(/volume-level-1/);
     });
-    test('is muted', ()=>{
+    test('is muted at 0', ()=>{
         expect(formatVolumeIconPath(0)).toMatch(/volume-level-0/);
     });
 });
